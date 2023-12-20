9a3b58152b2aac76a86d6369a55aa5803b99fdef
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-12-20 19:03:00 +0530

    Update bt.java

--- bt.java
+++ bt.java
@@ -41,6 +41,34 @@ class Main {
             int rh = height(root.right);
             return Math.max(lh,rh) + 1;
         }
+        public static int count(Node root){
+            if(root==null){
+                return 0;
+            }
+            int leftCount = count(root.left);
+            int rightCount = count(root.right);
+            return leftCount + rightCount + 1;
+        }
+        public static int sum(Node root){
+            if(root==null){
+                return 0;
+            }
+            int leftSum = sum(root.left);
+            int rightSum = sum(root.right);
+            return leftSum + rightSum + root.data;
+        }
+        public static int diameter(Node root){
+            if(root==null){
+                return 0;
+            }
+            int leftDiam = diameter(root.left);
+            int leftHeight = height(root.left);
+            int rightDiam = diameter(root.right);
+            int rightHeight = height(root.right);
+
+            int selfDiam = leftHeight + rightHeight + 1;
+            return Math.max(selfDiam, Math.max(leftDiam,rightDiam));
+        }
         public static void DFS(Node root){
             if(root==null){
                 return;
@@ -74,6 +102,9 @@ class Main {
         BT tree = new BT();
         Node root = tree.createNode(nodes);
         System.out.println(height(root));
+        System.out.println(count(root));
+        System.out.println(sum(root));
+        System.out.println(diameter(root));
         tree.DFS(root);
     }
 }
