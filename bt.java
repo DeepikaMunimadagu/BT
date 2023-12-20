72c34a7df1e94b12143105873918d745273efd33
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-12-20 12:49:36 +0530

    Update bt.java

--- bt.java
+++ bt.java
@@ -33,6 +33,14 @@ class Main {
             System.out.println(root.data+" ");
             inorder(root.right);
         }
+        public static int height(Node root){
+            if(root==null){
+                return 0;
+            }
+            int lh = height(root.left);
+            int rh = height(root.right);
+            return Math.max(lh,rh) + 1;
+        }
         public static void DFS(Node root){
             if(root==null){
                 return;
@@ -65,6 +73,7 @@ class Main {
         int nodes[] = {1,2,3,4,5,6,7,8,9,10};
         BT tree = new BT();
         Node root = tree.createNode(nodes);
+        System.out.println(height(root));
         tree.DFS(root);
     }
 }
