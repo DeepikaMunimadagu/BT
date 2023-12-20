b134cb23435e7fae94fd27f071a0c54162683fb1
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-12-20 12:45:04 +0530

    Create bt.java

---
+++ bt.java
@@ -0,0 +1,70 @@
+import java.util.*;
+class Main {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BT{
+        static int idx = -1;
+        public static Node createNode(int nodes[]) {
+            idx++;
+            if (idx >= nodes.length || nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = createNode(nodes);
+            newNode.right = createNode(nodes);
+
+            return newNode;
+        }
+        
+        public static void inorder(Node root){
+            if(root == null){
+                return;
+            }
+            inorder(root.left);
+            System.out.println(root.data+" ");
+            inorder(root.right);
+        }
+        public static void DFS(Node root){
+            if(root==null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1,2,3,4,5,6,7,8,9,10};
+        BT tree = new BT();
+        Node root = tree.createNode(nodes);
+        tree.DFS(root);
+    }
+}
